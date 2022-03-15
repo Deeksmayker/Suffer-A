26f5c64f2074a1761e5d08072cbf28ec46d902bf
Deeksmayker <lokkins334@gmail.com>
2022-03-15 18:36:39 +0500

    1a

---
+++ Assets/Scripts/Model/SceneChanger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    public class SceneChanger : MonoBehaviour
+    {
+        [SerializeField] private Scene nextScene;
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            
+        }
+    }
+}
