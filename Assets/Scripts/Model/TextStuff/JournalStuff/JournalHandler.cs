ab19dda7259f4bf1de15bd89e0cf935a8a93125c
Deeksmayker <lokkins334@gmail.com>
2022-05-28 04:24:26 +0500

    more interface improvements

---
+++ Assets/Scripts/Model/TextStuff/JournalStuff/JournalHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace.TextStuff.JournalStuff
+{
+    public class JournalHandler : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> journalButtons;
+        
+        
+    }
+}
