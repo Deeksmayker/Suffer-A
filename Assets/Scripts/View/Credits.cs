8f0bfadbea9ea2be1c14a3afc1e6597ee0b62379
Deeksmayker <lokkins334@gmail.com>
2022-06-01 17:58:27 +0500

    fin i think

---
+++ Assets/Scripts/View/Credits.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using Camera;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+namespace DefaultNamespace
+{
+    public class Credits : MonoBehaviour
+    {
+        [SerializeField] private GameObject video;
+        private GameObject _vid;
+        
+        public IEnumerator PlayCredits()
+        {
+            _vid = Instantiate(video);
+            _vid.GetComponent<VideoPlayer>().targetCamera = UnityEngine.Camera.main;
+            yield return new WaitForSeconds(24);
+            Destroy(_vid);
+            Destroy(FindObjectOfType<CameraStuff>().gameObject);
+
+            SceneManager.LoadScene(0);
+        }
+    }
+}
